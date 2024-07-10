ea5b6cf0ea4b8511144f38c594775c8c74aa8398
Nikola Kolev <nickkolev06@abv.bg>
2024-07-10 23:25:55 +0300

    finished demo

--- 01.todo-list/client/src/components/Main.jsx
+++ 01.todo-list/client/src/components/Main.jsx
@@ -1,47 +1,64 @@
 import { useEffect, useState } from "react";
 import TodoItem from "./TodoItem";
+import Spinner from "./Spinner";
+import React from 'react';
 
 export default function Main() {
-  const [todos, setTodos] = useState([]);
+	const [todos, setTodos] = useState([]);
+	const [pending, setPending] = useState(true);
 
-  useEffect(() => {
-    fetch("http://localhost:3030/jsonstore/todos")
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-      });	
-  }, []);
+	useEffect(() => {
+		fetch("http://localhost:3030/jsonstore/todos")
+			.then((res) => res.json())
+			.then((result) => {
+				const data = Object.values(result);
+				setTodos(data);
+				setPending(false);
+			});	
+	}, []);
 
-    return (
-        <main className="main">
-            <section className="todo-list-container">
-                <h1>Todo List</h1>
+	const todoItemChangeHandler = (todoId) => {
+		setTodos((oldTodos) => oldTodos.map(todo => todo._id === todoId 
+			? {...todo, isCompleted: !todo.isCompleted} 
+			: todo
+		));
+	}
 
-                <div className="add-btn-container">
-                    <button className="btn">+ Add new Todo</button>
-                </div>
+	return (
+			<main className="main">
+					<section className="todo-list-container">
+							<h1>Todo List</h1>
 
-                <div className="table-wrapper">
-                    {/* SPINNER */}
+							<div className="add-btn-container">
+									<button className="btn">+ Add new Todo</button>
+							</div>
 
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th className="table-header-task">Task</th>
-                                <th className="table-header-status">Status</th>
-                                <th className="table-header-action">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <TodoItem />
-                            <TodoItem />
-                            <TodoItem />
-                            <TodoItem />
+							<div className="table-wrapper">
+								{pending && <Spinner />}
+								{/* SPINNER */}
 
-                        </tbody>
-                    </table>
-                </div>
-            </section>
-        </main>
-    );
+								<table className="table">
+										<thead>
+												<tr>
+														<th className="table-header-task">Task</th>
+														<th className="table-header-status">Status</th>
+														<th className="table-header-action">Action</th>
+												</tr>
+										</thead>
+										<tbody>
+												{todos.map(todo => 
+													<TodoItem 
+														key={todo._id} 
+														id={todo._id}
+														text={todo.text}
+														isCompleted={todo.isCompleted}
+														onStatusChange={todoItemChangeHandler}
+													/>
+												)}
+										</tbody>
+								</table>
+							</div>
+					</section>
+			</main>
+	);
 }
--- 01.todo-list/client/src/components/TodoItem.jsx
+++ 01.todo-list/client/src/components/TodoItem.jsx
@@ -1,10 +1,18 @@
-export default function TodoItem() {
+export default function TodoItem(props) {
+    const classes = ['todo'];
+
+    console.log(props);
+
+    if (props.isCompleted) {
+        classes.push('is-completed');  
+    } 
+
     return (
-        <tr className="todo is-completed">
-            <td>Give dog a bath</td>
+        <tr className={classes.join(' ')}>
+            <td>{props.text}</td>
             <td>Complete</td>
             <td className="todo-action">
-                <button className="btn todo-btn">
+                <button className="btn todo-btn" onClick={() => props.onStatusChange(props.id)}>
                     Change status
                 </button>
             </td>
